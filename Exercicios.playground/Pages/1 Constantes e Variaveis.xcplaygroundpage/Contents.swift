5bd02796f45a401f50cea4dd93f0220e88163a80
luizv <iam.luizv@gmail.com>
2023-05-04 13:09:58 -0300

    corrige probleminha do commit original

--- Exercicios.playground/Pages/1 Constantes e Variaveis.xcplaygroundpage/Contents.swift
+++ Exercicios.playground/Pages/1 Constantes e Variaveis.xcplaygroundpage/Contents.swift
@@ -32,7 +32,6 @@ import UIKit
 
     func converteFParaC(...) {}
  */
-let a = 1+2
 
 /*:
  ### 6. O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica de um carro, calcular e escrever o custo final ao consumidor. [🐝]
